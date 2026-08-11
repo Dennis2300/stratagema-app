d6281b0b76fbdd9c9c69c0a97c6daf09e1180816
Densanity <denniszixiang@gmail.com>
2026-08-11 06:20:05 +0200

    added game version in landing page

---
+++ app/components/Homepage/GameVersion.vue
@@ -0,0 +1,60 @@
+<template>
+  <article>
+    <div v-if="pending">
+      <p>Loading</p>
+    </div>
+
+    <div v-else-if="error">
+      <p>{{ error.message }}</p>
+    </div>
+
+    <div v-else-if="currentVersion">
+      <figure class="relative aura text-white">
+        <img
+          class="w-full h-full object-cover rounded-2xl"
+          :src="currentVersion.img_url"
+          alt=""
+        />
+        <div
+          class="absolute inset-0 bg-linear-to-t from-black via-black/50 to-transparent rounded-2xl"
+        ></div>
+        <figcaption class="absolute bottom-5 left-5">
+          <span>Genshin Impact | 原神</span>
+          <h1>{{ currentVersion.name }}</h1>
+          <p class="text-lg mt-2">
+            Version {{ currentVersion.version_number }} is available now!
+          </p>
+        </figcaption>
+      </figure>
+    </div>
+
+    <div v-else>
+      <p>Empty Fallback</p>
+    </div>
+  </article>
+</template>
+
+<script setup>
+const supabase = useSupabaseClient();
+
+const {
+  data: currentVersion,
+  pending,
+  error,
+} = await useAsyncData("game_version", async () => {
+  const now = new Date().toISOString();
+  // For Testing:
+  //const now = new Date("2026-08-15T00:00:00Z").toISOString();
+
+  const { data, error } = await supabase
+    .schema("genshin_impact")
+    .from("game_versions")
+    .select("*")
+    .lte("start_date", now)
+    .gte("end_date", now)
+    .maybeSingle();
+
+  if (error) throw error;
+  return data;
+});
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,32 +1,7 @@
 <template>
   <div>
-    <h1>Welcome to STRATAGEMA APP</h1>
-    <div v-if="pending">Loading characters...</div>
-    <div v-else-if="error">Something went wrong: {{ error.message }}</div>
-    <div v-else>
-      <div v-for="character in characters" :key="character.id" class="card">
-        <h3>{{ character.name }}</h3>
-        <p>{{ character.description }}</p>
-      </div>
-    </div>
+    <HomepageGameVersion />
   </div>
 </template>
 
-<script setup>
-const supabase = useSupabaseClient();
-
-const {
-  data: characters,
-  pending,
-  error,
-  refresh,
-} = await useAsyncData("characters", async () => {
-  const { data, error } = await supabase
-    .schema("genshin_impact")
-    .from("characters")
-    .select("*");
-
-  if (error) throw error;
-  return data;
-});
-</script>
+<script setup></script>
